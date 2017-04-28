4420ae8b9de71ee22dd631e2c261700bb613b5a6
Ryota Yoshikawa <ryota-yoshikawa@cookpad.com>
2017-04-28 20:01:24 +0900

    Remove resources because it is not model

--- app/resources/application_resource.rb
+++
@@ -1,9 +0,0 @@
-class ApplicationResource
-  include JsonWorld::DSL
-
-  title "Rice Server API v1"
-  description "Rice Server API v1 interface document written in JSON Hyper Schema draft v4"
-
-  property :recipes, links: true, type: RecipeResource
-  link href: "https://xn--38j8bv87v8z2b18b", rel: "self"
-end
